737c4cd98e8ca0286d9eaff491f64e54d0ce5186
OBrutus <vaishnavani7@gmail.com>
2026-05-18 22:57:29 +0530

    fix: update TARGET_INSTALL_DIR to use a variable and adjust path handling for setup

--- install.py
+++ install.py
@@ -2,6 +2,8 @@ from sys import platform
 import os
 import sys
 
+TARGET_INSTALL_DIR = "~/.prepare-code"
+# TARGET_INSTALL_DIR = "/tmp/prepare-code"
 
 expressInstall = False
 ConfigFileName = "config.yaml"
@@ -28,6 +30,8 @@ def clone_and_build():
         os.system("rm -rf /tmp/prepare-code")
 
     tmp_cmd = "cd /tmp && git clone https://github.com/OBrutus/prepare-code.git"
+    # tmp_cmd = "cd /tmp && cp -r /Users/obrutus/kode/prepare-code /tmp/"
+    
     status_code = os.system(tmp_cmd)
 
     if status_code != 0:
@@ -49,19 +53,21 @@ def add_to_path():
     if platform == "darwin":
         shell_config = os.path.expanduser("~/.zshrc")
     with open(shell_config, "a") as f:
-        f.write('\nexport PATH="$HOME/.prepare-code:$PATH"\n')
+        f.write('\nexport PATH=' + TARGET_INSTALL_DIR + '":$PATH"\n')
     print(f"Added to PATH in {shell_config}. Please restart your terminal "
           "or run 'source {shell_config}' to apply changes.")
 
 
 def setup_nix():
-    if not remove_dir_if_exists("~/.prepare-code", "Do you want to remove it "
+    if not remove_dir_if_exists(TARGET_INSTALL_DIR, "Do you want to remove it "
                                 "and continue? (Y/n)"):
         return None
+
     if not clone_and_build():
         return None
-    os.system("mkdir -p ~/.prepare-code")
-    os.system("mv /tmp/prepare-code/* ~/.prepare-code")
+
+    os.system("mkdir -p "+TARGET_INSTALL_DIR)
+    os.system("mv /tmp/prepare-code/* " + TARGET_INSTALL_DIR)
     print("Setup completed to clone.")
     if expressInstall:
         choice = 'y'
@@ -70,7 +76,7 @@ def setup_nix():
         choice = input(choice_prompt).strip().lower() or 'y'
     if choice == 'y':
         add_to_path()
-    return os.path.expanduser("~/.prepare-code")
+    return os.path.expanduser(TARGET_INSTALL_DIR)
 
 
 def setup():
@@ -91,6 +97,8 @@ def setup():
 def create_config_file(install_dir: str):
     content = "install_dir: " + install_dir + "\n"
     content += "bypass_prompt: " + "false" + "\n"
+    content += "preferred_editor: nvim \n"
+    content += "open_in_editor: false \n"
 
     file = open(os.path.join(install_dir, ConfigFileName), 'w')
     file.write(content)
