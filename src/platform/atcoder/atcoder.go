4b28f81dc3f72c70559a1734b413b91d023ca5b6
OBrutus <vaishnavani7@gmail.com>
2026-06-13 23:36:30 +0530

    feat: implement AtCoder platform support with URL extraction and testing

---
+++ src/platform/atcoder/atcoder.go
@@ -0,0 +1,76 @@
+package atcoder
+
+import (
+	"errors"
+	"prepare-code/src/constants"
+	"prepare-code/src/types"
+	"strings"
+)
+
+var ErrInvalidAtCoderUrl = errors.New("invalid url of AtCoder")
+
+const urlFormat string = "https://atcoder.jp/contests/abc461/tasks/abc461_a"
+
+const codeNameDelimiter string = "-"
+
+type AtCoderPlatform struct {
+	name     types.PlatformName
+	codeName string
+}
+
+func (ac AtCoderPlatform) GetPlatformName() types.PlatformName {
+	return constants.PlatformAtCoder
+}
+
+func (ac AtCoderPlatform) GetCodeName() string {
+	return ac.codeName
+}
+
+func (ac AtCoderPlatform) GetPlatform(url string) (types.Platform, error) {
+	return GetPlatform(url)
+}
+
+// This is the main method
+func GetPlatform(url string) (types.Platform, error) {
+	extractedCodeName, err := extractCodeName(url)
+	if err != nil {
+		return nil, err
+	}
+
+	return AtCoderPlatform{
+		name:     constants.PlatformCodeforces,
+		codeName: extractedCodeName, // placeholder for actual extraction logic
+	}, nil
+}
+
+func extractCodeName(url string) (string, error) {
+	// https://atcoder.jp/contests/abc461/tasks/abc461_a
+	if len(url) < len("atcoder.jp") {
+		return "", ErrInvalidAtCoderUrl
+	}
+
+	i := 0
+	const prefix string = "atcoder.jp/contests/"
+	for i+len(prefix) < len(url) {
+		if url[i:i+len(prefix)] == prefix {
+			break
+		}
+		i++
+	}
+
+	i += len(prefix)
+	if i == len(url) {
+		return "", ErrInvalidAtCoderUrl
+	}
+
+	prefixRemovedString := url[i:]
+	suffix := strings.Split(prefixRemovedString, "/")
+	if len(suffix) < 3 {
+		return "", ErrInvalidAtCoderUrl
+	}
+
+	// contestNumber := suffix[0]
+	qeustionCode := strings.ToUpper(suffix[2])
+
+	return qeustionCode, nil
+}
